5a382784ee7cc5a8f3aed81f57ec8d4ca3603d82
neelshinde-git <66645545+neelshinde-git@users.noreply.github.com>
2020-09-06 15:39:30 +0530

    Add files via upload

---
+++ precache-manifest.35ba0962809309b79d637a86afd64893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b23c9133d8f5f817e52c686c57710f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6e7157054257c6868f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fc26139a9bbd261e0ff9",
+    "url": "/static/css/main.e50ee92a.chunk.css"
+  },
+  {
+    "revision": "1a6e7157054257c6868f",
+    "url": "/static/js/2.7f8bbc54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f8bbc54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc26139a9bbd261e0ff9",
+    "url": "/static/js/main.efa664d2.chunk.js"
+  },
+  {
+    "revision": "44a72db968031c2109ad",
+    "url": "/static/js/runtime-main.7841fcae.js"
+  }
+]);
